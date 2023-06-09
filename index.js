d5dd00fdb0667f3f32791c4eace0f25146df3cfe
Nimish G J <84202076+nimishgj@users.noreply.github.com>
2023-06-09 16:27:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const goto = (txt)=>{
+    window.location.href=`#${txt}`
+}
+
+
+const instagram=()=>{
+    window.open("https://www.instagram.com/nimish.gj/","_blank")
+}
+
+const github=()=>{
+    window.open("https://github.com/nimishgj","_blank")
+}
+
+const linkedin=()=>{
+    window.open("https://www.linkedin.com/in/nimishgj","_blank")
+}
+
+const twitter=()=>{
+    window.open("https://twitter.com/g_nimish","_blank")
+}
